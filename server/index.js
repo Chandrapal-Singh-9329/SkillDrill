fc568e932084da50b215b20df179cd68e862abbc
Chandrapal <chandrapalsingh91711@gmail.com>
2026-06-17 14:58:22 +0530

    Add pricing plans with Razorpay payment integration and credit top-up flow

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import cors from 'cors';
 import authRouter from './routes/authRoute.js';
 import userRouter from './routes/userRoute.js';
 import interviewRouter from './routes/interviewRoute.js';
+import paymentRouter from "./routes/paymentRoute.js";
 
 
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use('/api/auth' , authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/interview' , interviewRouter)
+app.use("/api/payment", paymentRouter)
 
 
 const PORT = process.env.PORT || 8000;
